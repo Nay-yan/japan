a0ff0349ec0802b41895c00d045bcab1bd076e29
WaiYan <aa8184792@gmial.com>
2021-10-11 22:36:13 +0630

    second

--- Hiragana/D_pron/d_pron.js
+++ Hiragana/D_pron/d_pron.js
@@ -3,8 +3,8 @@ console.log(Math.floor(total));
 
 document.getElementById("StartBtn").addEventListener("click", function() {
     var Her = Math.floor(Math.random() * 25) + 1;
-    document.getElementById("Hiragana").src = `../../img/Hirag/H- Dakuon/${Her}.png`;
-    document.getElementById("Hint").src = `../../img/Hirag/D-Dakuon Romaji/${Her}.png`;
+    document.getElementById("Hiragana").src = `../../img/Hirag/D- Dakuon/${Her}.png`;
+    document.getElementById("Hint").src = `../../img/Hirag/D-Romaji/${Her}.png`;
     document.getElementById("Total").textContent = ++total;
     console.log(Her);
 });
